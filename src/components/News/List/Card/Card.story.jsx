63a6a003d60d2eb5029cdb401a9e0ee6444a369b
Marco Barão <marco.barao@outlook.com>
2019-02-04 10:34:24 -0200

    Solves defaultThumbnail problem

--- src/components/News/List/Card/Card.story.jsx
+++ src/components/News/List/Card/Card.story.jsx
@@ -10,20 +10,28 @@ storiesOf('NewsListCard component', module)
   ))
   .add('with required props', () => (
     <NewsListCard
-      id="sport/live/2019/feb/02/ireland-v-england-six-nations-live"
-      sectionName="Sport"
-      webTitle="Ireland v England: Six Nations 2019 – live!"
-      webPublicationDate="02/02/2019 11:00:37"
-      productionOffice="UK"
+      article={{
+        id: 'sport/live/2019/feb/02/ireland-v-england-six-nations-live',
+        webTitle: 'Ireland v England: Six Nations 2019 – live!',
+        webPublicationDate: '02/02/2019 11:00:37',
+        sectionName: 'Sport',
+        fields: {
+          publication: 'theguardian.com',
+        },
+      }}
     />
   ))
   .add('with all props', () => (
     <NewsListCard
-      id="sport/live/2019/feb/02/ireland-v-england-six-nations-live"
-      sectionName="Sport"
-      webTitle="Ireland v England: Six Nations 2019 – live!"
-      webPublicationDate="02/02/2019 11:00:37"
-      thumbnail="https://media.guim.co.uk/89bb00615f5477d6ef026da116200360b43a9107/0_100_4260_2556/500.jpg"
-      productionOffice="UK"
+      article={{
+        id: 'sport/live/2019/feb/02/ireland-v-england-six-nations-live',
+        webTitle: 'Ireland v England: Six Nations 2019 – live!',
+        webPublicationDate: '02/02/2019 11:00:37',
+        sectionName: 'Sport',
+        fields: {
+          publication: 'theguardian.com',
+          thumbnail: 'https://media.guim.co.uk/89bb00615f5477d6ef026da116200360b43a9107/0_100_4260_2556/500.jpg',
+        },
+      }}
     />
   ));
